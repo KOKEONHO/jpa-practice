fd09646bf8cc5be54b2b534deaf6fb13e4d2bc4a
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-13 00:58:22 +0900

    Feat: JpaPracticeApplicationTests.java 생성

---
+++ src/test/java/practice/jpapractice/JpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.jpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
